1532b8b8db139cf64f6f51db8e30714c70c93432
rangesh <rangeshtv14@gmail.com>
2021-10-17 20:20:13 +0530

    this is commit from IST

--- src/test/java/Maventest/MavenJava/SeleniumTest.java
+++ src/test/java/Maventest/MavenJava/SeleniumTest.java
@@ -7,6 +7,8 @@ public class SeleniumTest {
 	@Test
 	public void BrowserAutomation() {
 		System.out.println("Maven started");
+		System.out.println("Git started");
+		System.out.println("Git2 started");
 
 	}
 
