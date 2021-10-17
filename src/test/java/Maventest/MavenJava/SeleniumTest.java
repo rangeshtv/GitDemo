8441b193196ad049191b9895c29ef3f6ab328b3f
rangesh <rangeshtv14@gmail.com>
2021-10-17 19:54:39 +0530

    First Commit

---
+++ src/test/java/Maventest/MavenJava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package Maventest.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("Maven started");
+
+	}
+
+	@Test
+	public void AIAutomation() {
+
+	}
+}
